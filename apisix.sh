22468167c379d0847241fe2e5746a3037f9b2b15
WenMing <moonbingbing@gmail.com>
2019-09-29 09:55:18 +0800

    Update apisix.sh

--- apisix.sh
+++ apisix.sh
@@ -7,4 +7,4 @@ gem install --no-ri --no-rdoc fpm
 cp /media/psf/Home/work/apisix/conf/nginx.conf /tmp/apisix/usr/share/lua/5.1/apisix/conf/nginx.conf
 cp -r /media/psf/Home/work/apisix/lua/ /tmp/apisix/usr/share/lua/5.1/apisix/
 
-fpm -f -s dir -t rpm -n apisix -a all -v 0.1 --iteration 2 -d 'openresty >= 1.15.8.1' -d 'etcd >= 3.3' --description 'APISix is a distributed gateway for APIs and Microservices, focused on high performance and reliability.' --license "ASL 2.0"  -C /tmp/apisix/ -p /tmp/rpm/ --url 'https://www.iresty.com'
+fpm -f -s dir -t rpm -n apisix -a all -v 0.1 --iteration 2 -d 'openresty >= 1.15.8.1' --description 'APISix is a distributed gateway for APIs and Microservices, focused on high performance and reliability.' --license "ASL 2.0"  -C /tmp/apisix/ -p /tmp/rpm/ --url 'https://www.iresty.com'
