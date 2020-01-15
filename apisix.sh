4acc3ef69bc447bf8e22413836fedb7cea6abce4
WenMing <moonbingbing@gmail.com>
2020-01-15 19:35:30 +0800

    Update apisix.sh

--- apisix.sh
+++ apisix.sh
@@ -1,4 +1,4 @@
-version=0.7
+version=1.0
 iteration=0
 
 sudo luarocks remove apisix
@@ -9,7 +9,6 @@ wget https://github.com/iresty/apisix/raw/master/rockspec/apisix-$version-0.rock
 sudo luarocks install apisix-$version-0.rockspec --tree=/tmp/apisix/usr/local/apisix/deps
 sudo chown -R $USER:$USER /tmp/apisix
 
-
 mkdir -p /tmp/apisix/usr/bin/
 mkdir -p /tmp/rpm/
 
@@ -19,12 +18,12 @@ sed -i "1s@.*@$bin@" /tmp/apisix/usr/bin/apisix
 
 # for conf, log and dashboard dirs
 cp -r /usr/local/apisix/* /tmp/apisix/usr/local/apisix/
+mkdir /tmp/apisix/usr/local/apisix/dashboard
+cp -r /home/centos/incubator-apisix-dashboard/dist/* /tmp/apisix/usr/local/apisix/dashboard
 
 # code base
 mv /tmp/apisix/usr/local/apisix/deps/share/lua/5.1/apisix/lua /tmp/apisix/usr/local/apisix
 
-cp /tmp/apisix/usr/local/apisix/deps/lib64/luarocks/rocks/apisix/$version-$iteration/conf/COPYRIGHT /tmp/apisix/usr/local/apisix
-cp /tmp/apisix/usr/local/apisix/deps/lib64/luarocks/rocks/apisix/0.8-0/conf/README.md /tmp/apisix/usr/local/apisix
 rm -rf /tmp/apisix/usr/local/apisix/deps/lib64/luarocks
 
-fpm -f -s dir -t rpm -n apisix -a all -v $version --iteration $iteration -d 'openresty >= 1.15.8.1' --description 'APISIX is a distributed gateway for APIs and Microservices, focused on high performance and reliability.' --license "ASL 2.0"  -C /tmp/apisix/ -p /tmp/rpm/ --url 'https://www.iresty.com'
+fpm -f -s dir -t rpm -n apisix -a all -v $version --iteration $iteration -d 'openresty >= 1.15.8.1' --description 'Apache APISIX is a distributed gateway for APIs and Microservices, focused on high performance and reliability.' --license "ASL 2.0"  -C /tmp/apisix/ -p /tmp/rpm/ --url 'https://www.iresty.com'
