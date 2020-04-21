c4211e9aabd6d7a93c5d29cdb915830cb52f7a9d
WenMing <moonbingbing@gmail.com>
2020-04-21 14:43:52 +0800

    updated 1.2 version.

--- run.sh
+++ run.sh
@@ -1,29 +1,40 @@
-# version=1.1
-version=master
+version=1.2
+# version=master
 iteration=0
 
-sudo luarocks remove apisix
+sudo luarocks remove apisix --local
 sudo rm -rf /usr/local/apisix/
 rm -rf /tmp/apisix
 
-wget https://github.com/iresty/apisix/raw/master/rockspec/apisix-$version-0.rockspec
-sudo luarocks install apisix-$version-0.rockspec --tree=/tmp/apisix/usr/local/apisix/deps
+wget https://github.com/apache/incubator-apisix/raw/master/rockspec/apisix-$version-0.rockspec
+sudo luarocks install apisix-$version-0.rockspec --tree=/tmp/apisix/usr/local/apisix/deps --local
 sudo chown -R $USER:$USER /tmp/apisix
 
 mkdir -p /tmp/apisix/usr/bin/
 mkdir -p /tmp/rpm/
 
-cp /tmp/apisix/usr/local/apisix/deps/lib64/luarocks/rocks/apisix/$version-0/bin/apisix /tmp/apisix/usr/bin/
-bin='#! /usr/local/openresty/luajit/bin/luajit\npackage.path = "/usr/local/apisix/lua/?.lua;" .. package.path'
+cp /tmp/apisix/usr/local/apisix/deps/lib/luarocks/rocks-5.1/apisix/$version-0/bin/apisix /tmp/apisix/usr/bin/
+bin='#! /usr/local/openresty/luajit/bin/luajit\npackage.path = "/usr/local/apisix/?.lua;" .. package.path'
 sed -i "1s@.*@$bin@" /tmp/apisix/usr/bin/apisix
 
-# for conf, log and dashboard dirs
+# for conf, log
 cp -r /usr/local/apisix/* /tmp/apisix/usr/local/apisix/
-# mkdir /tmp/apisix/usr/local/apisix/dashboard
-# cp -r /home/centos/incubator-apisix-dashboard/dist/* /tmp/apisix/usr/local/apisix/dashboard
+
+# for dashboard
+git clone https://github.com/apache/incubator-apisix.git
+cd incubator-apisix
+git checkout tags/$version -b $version
+git submodule update --init --recursive
+cd dashboard
+yarn && yarn build:prod
+
+mkdir /tmp/apisix/usr/local/apisix/dashboard
+cp -r dist/* /tmp/apisix/usr/local/apisix/dashboard
+cd ../..
+rm -rf incubator-apisix
 
 # code base
-mv /tmp/apisix/usr/local/apisix/deps/share/lua/5.1/apisix/lua /tmp/apisix/usr/local/apisix
+mv /tmp/apisix/usr/local/apisix/deps/share/lua/5.1/apisix /tmp/apisix/usr/local/apisix/
 
 rm -rf /tmp/apisix/usr/local/apisix/deps/lib64/luarocks
 
