fea86ffcd7444faa4cedda088bbc56f2f79f2abd
idbeta <idbeta@gmail.com>
2021-01-14 15:53:04 +0800

    fixed bug

--- build-deb.sh
+++ build-deb.sh
@@ -75,14 +75,15 @@ build_apisix() {
 
 install_dependencies_dashboard() {
     # install base dependencies, nodejs, yarn
+    apt-get update
     apt-get -y install wget curl git gcc make
-    sh -c "$(curl -fsSL https://rpm.nodesource.com/setup_10.x)"
+    curl -sL https://deb.nodesource.com/setup_10.x | bash -
     apt-get install -y nodejs
 
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
     echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-    apt update
-    apt install yarn
+    apt-get update
+    apt-get install yarn
 
     # install golang
     wget https://dl.google.com/go/go1.15.2.linux-amd64.tar.gz 
