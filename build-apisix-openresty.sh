fc4a012abcbae41b981b8616a2ac90593b2feef6
spacewander <spacewanderlzx@gmail.com>
2021-03-29 19:21:21 +0800

    feat: build apisix-nginx-module
    And allow the script to use current directory's code instead of download
    it

--- build-apisix-openresty.sh
+++ build-apisix-openresty.sh
@@ -14,17 +14,33 @@ else
     OR_PREFIX=${OR_PREFIX:="/usr/local/openresty"}
 fi
 
+prev_workdir="$PWD"
+repo=$(basename "$prev_workdir")
 workdir=$(mktemp -d)
 cd "$workdir" || exit 1
 
 wget https://openresty.org/download/openresty-1.19.3.1.tar.gz
 tar -zxvpf openresty-1.19.3.1.tar.gz
 
-git clone --depth=1 $ngx_multi_upstream_module_ver \
-    https://github.com/api7/ngx_multi_upstream_module.git
-git clone --depth=1 $mod_dubbo_ver \
-    https://github.com/api7/mod_dubbo.git
-git clone --depth=1 https://github.com/api7/apisix-nginx-module.git
+if [ "$repo" == ngx_multi_upstream_module ]; then
+    cp -r "$prev_workdir" .
+else
+    git clone --depth=1 $ngx_multi_upstream_module_ver \
+        https://github.com/api7/ngx_multi_upstream_module.git
+fi
+
+if [ "$repo" == mod_dubbo ]; then
+    cp -r "$prev_workdir" .
+else
+    git clone --depth=1 $mod_dubbo_ver \
+        https://github.com/api7/mod_dubbo.git
+fi
+
+if [ "$repo" == apisix-nginx-module ]; then
+    cp -r "$prev_workdir" .
+else
+    git clone --depth=1 https://github.com/api7/apisix-nginx-module.git
+fi
 
 cd ngx_multi_upstream_module || exit 1
 ./patch.sh ../openresty-1.19.3.1
@@ -36,7 +52,9 @@ cd ../..
 
 cd openresty-1.19.3.1 || exit 1
 ./configure --prefix="$OR_PREFIX" \
-    --add-module=../mod_dubbo --add-module=../ngx_multi_upstream_module \
+    --add-module=../mod_dubbo \
+    --add-module=../ngx_multi_upstream_module \
+    --add-module=../apisix-nginx-module \
     $debug_args \
     --with-poll_module \
     --with-pcre-jit \
@@ -67,3 +85,7 @@ cd openresty-1.19.3.1 || exit 1
     --with-luajit-xcflags='-DLUAJIT_NUMMODE=2 -DLUAJIT_ENABLE_LUA52COMPAT'
 make
 sudo make install
+cd ..
+
+cd apisix-nginx-module || exit 1
+sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
