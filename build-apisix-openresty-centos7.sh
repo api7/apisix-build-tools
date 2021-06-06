612aa2e76d9126edcc068f39a4b894689f06b4e0
tzssangglass <tzssangglass@gmail.com>
2021-06-07 00:54:24 +0800

    remove platform tag

--- build-apisix-openresty-centos7.sh
+++ build-apisix-openresty-centos7.sh
@@ -4,7 +4,7 @@ set -x
 
 yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
 yum -y install pcre-devel openssl-devel gcc curl gcc-c++ patch
-yum -y install openresty-openssl111-devel.x86_64
+yum -y install openresty-openssl111-devel
 
 export openssl_prefix=/usr/local/openresty/openssl111
 export cc_opt="-I${openssl_prefix}/include"
