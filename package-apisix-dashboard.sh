28d1ce86fa2512c53d3f979221ebf183a913c5b7
nic-chen <johz@163.com>
2021-07-09 23:33:16 +0800

    fix: output target should be a dir not a file

--- package-apisix-dashboard.sh
+++ package-apisix-dashboard.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 set -x
+
+mkdir /output
 fpm -f -s dir -t rpm \
 	-n apisix-dashboard \
 	-a "$(uname -i)" \
@@ -9,5 +11,5 @@ fpm -f -s dir -t rpm \
 	--description 'Apache APISIX Dashboard is designed to make it as easy as possible for users to operate Apache APISIX through a frontend interface.'  \
 	--license "ASL 2.0" \
 	-C /tmp/build/output/apisix/dashboard/ \
-	-p /output \
+	-p /output/ \
 	--url 'https://github.com/apache/apisix-dashboard'
