eae22965abc039bc05a2ed6ea94891df833f789a
Kushal-kothari <kushalkothari285@gmail.com>
2021-09-24 20:04:03 +0530

    feat: add ngx_http_geoip2_module

--- utils/build-common.sh
+++ utils/build-common.sh
@@ -6,6 +6,7 @@ build_apisix_openresty_rpm() {
     yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
     yum -y install gcc gcc-c++ patch wget git make sudo
     yum -y install openresty-openssl111-devel openresty-pcre-devel openresty-zlib-devel
+    yum -y install libmaxminddb-devel
 
     export_openresty_variables
     ./build-apisix-openresty.sh
@@ -19,6 +20,7 @@ build_apisix_openresty_deb() {
     echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/openresty.list
     DEBIAN_FRONTEND=noninteractive apt-get update
     DEBIAN_FRONTEND=noninteractive apt-get install -y openresty-openssl111-dev openresty-pcre-dev openresty-zlib-dev
+    DEBIAN_FRONTEND=noninteractive apt-get -y install libmaxminddb-dev
 
     export_openresty_variables
     ./build-apisix-openresty.sh
@@ -28,7 +30,6 @@ export_openresty_variables() {
     export openssl_prefix=/usr/local/openresty/openssl111
     export zlib_prefix=/usr/local/openresty/zlib
     export pcre_prefix=/usr/local/openresty/pcre
-
     export cc_opt="-DNGX_LUA_ABORT_AT_PANIC -I${zlib_prefix}/include -I${pcre_prefix}/include -I${openssl_prefix}/include"
     export ld_opt="-L${zlib_prefix}/lib -L${pcre_prefix}/lib -L${openssl_prefix}/lib -Wl,-rpath,${zlib_prefix}/lib:${pcre_prefix}/lib:${openssl_prefix}/lib"
 }
