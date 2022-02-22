7d3153addf042867a1dc7033a2657c4d9a42193f
spacewander <spacewanderlzx@gmail.com>
2022-02-22 09:27:47 +0800

    chore: upgrade mod dubbo to 1.0.2
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- build-apisix-base.sh
+++ build-apisix-base.sh
@@ -12,7 +12,7 @@ if ([ $# -gt 0 ] && [ "$1" == "latest" ]) || [ "$version" == "latest" ]; then
     OR_PREFIX=${OR_PREFIX:="/usr/local/openresty-debug"}
 else
     ngx_multi_upstream_module_ver="-b 1.0.1"
-    mod_dubbo_ver="-b 1.0.1"
+    mod_dubbo_ver="-b 1.0.2"
     apisix_nginx_module_ver="-b 1.5.0"
     wasm_nginx_module_ver="-b 0.4.0"
     lua_var_nginx_module_ver="-b v0.5.2"
