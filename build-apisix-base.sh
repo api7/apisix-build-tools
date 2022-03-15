dbfef35eaea8dff7f3a92c890f734821e678b884
spacewander <spacewanderlzx@gmail.com>
2022-03-15 14:13:03 +0800

    feat: upgrade wasm-nginx-module to 0.5.0
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- build-apisix-base.sh
+++ build-apisix-base.sh
@@ -16,7 +16,7 @@ else
     ngx_multi_upstream_module_ver="-b 1.0.1"
     mod_dubbo_ver="-b 1.0.2"
     apisix_nginx_module_ver="-b 1.5.0"
-    wasm_nginx_module_ver="-b 0.4.0"
+    wasm_nginx_module_ver="-b 0.5.0"
     lua_var_nginx_module_ver="-b v0.5.2"
     debug_args=${debug_args:-}
     OR_PREFIX=${OR_PREFIX:="/usr/local/openresty"}
