317e2c44d3453ff27e00418d919a6b786673681b
leslie.tsang <leslie@apache.org>
2022-04-08 16:52:47 +0800

    chore: tweak code style

--- utils/publish-rpm-repo.sh
+++ utils/publish-rpm-repo.sh
@@ -8,6 +8,7 @@ set -x
 # OSS default config
 # =======================================
 VAR_ALIYUN_OSS_UTILS_VERSION=${VAR_ALIYUN_OSS_UTILS_VERSION:-1.7.10}
+VAR_ALIYUN_OSS_ENDPOINT=${VAR_ALIYUN_OSS_ENDPOINT:-endpoint=oss-cn-shenzhen.aliyuncs.com}
 
 # =======================================
 # GPG extension
@@ -23,49 +24,63 @@ func_gpg_config_init() {
 _EOC_
 }
 
+func_gpg_key_load() {
+    # ${1} gpg private key
+    # ${2} gpg key passphrase
+    gpg --import --pinentry-mode loopback --batch --passphrase-file "${2}" "${1}"
+
+    gpg --list-keys --fingerprint | grep "${GPG_MAIL}" -B 1 \
+    | tr -d ' ' | head -1 | awk 'BEGIN { FS = "\n" } ; { print $1":6:" }' \
+    | gpg --import-ownertrust
+}
+
 # =======================================
 # OSS extension
 # =======================================
 func_oss_utils_install() {
-    # ${1} - oss util version
+    # ${1} - OSS util version
     curl -o /usr/bin/ossutil64 "http://gosspublic.alicdn.com/ossutil/${1}/ossutil64"
     chmod 755 /usr/bin/ossutil64
 }
 
 func_oss_utils_credential_init() {
-    # ${1} - oss util config path
-    # ${2} - oss endpoint
-    # ${3} - ACCESS_KEY_ID
-    # ${4} - ACCESS_KEY_SECRET
-    cat <<_EOC_ > "${1}"
+    # ${1} - OSS endpoint
+    # ${2} - ACCESS_KEY_ID
+    # ${3} - ACCESS_KEY_SECRET
+    cat > ~/.ossutilconfig <<_EOC_
 [Credentials]
 language=EN
-endpoint=${2}
-accessKeyID=${3}
-accessKeySecret=${4}
+endpoint=${1}
+accessKeyID=${2}
+accessKeySecret=${3}
 _EOC_
 }
 
 # =======================================
 # OSS repo extension
 # =======================================
+func_repo_init() {
+    # ${1} - repo workbench path
+    mkdir -p "${1}"/centos/{7,8}/x86_64/Packages
+}
+
 func_repo_clone() {
     # ${1} - bucket name
-    # ${2} - oss path
+    # ${2} - OSS path
     # ${3} - target path
-    ossutil64 cp -r "oss://${1}/packages/${2}" "${3}"
+    ossutil64 cp -r -f "oss://${1}/packages/${2}" "${3}"
 }
 
 func_repo_backup() {
     # ${1} - bucket name
-    # ${2} - oss path
+    # ${2} - OSS path
     # ${3} - backup tag
     ossutil64 cp -r "oss://${1}/packages/${2}" "oss://${1}/packages/backup/${2}_${3}"
 }
 
 func_repo_backup_remove() {
     # ${1} - bucket name
-    # ${2} - oss path
+    # ${2} - OSS path
     # ${3} - backup tag
     ossutil64 rm -r -f "oss://${1}/packages/backup/${2}_${3}"
 }
@@ -82,22 +97,56 @@ func_repo_metadata_sign() {
 }
 
 func_repo_metadata_rebuild() {
-    createrepo .
+    # ${1} - repo parent path
+    find "${1}" -type d -name "x86_64" \
+        -exec echo "createrepo for: {}" \; \
+        -exec rm -rf {}/repodata \; \
+        -exec createrepo {} \;
 }
 
 func_repo_upload() {
     # ${1} - local path
     # ${2} - bucket name
-    # ${3} - oss path
+    # ${3} - OSS path
     ossutil64 rm -r -f "oss://${2}/packages/${3}"
-    ossutil64 cp -r ${1} "oss://${2}/packages/${3}"
+    ossutil64 cp -r "${1}" "oss://${2}/packages/${3}"
 }
 
+# =======================================
+# publish utils entry
+# =======================================
 case_opt=$1
 
 case ${case_opt} in
 init_oss_utils)
-    func_oss_utils_install ${VAR_ALIYUN_OSS_UTILS_VERSION}
-    func_oss_utils_credential_init ~/.ossutilconfig "endpoint=oss-cn-shenzhen.aliyuncs.com" "${ACCESS_KEY_ID}" "${ACCESS_KEY_SECRET}"
+    func_oss_utils_install "${VAR_ALIYUN_OSS_UTILS_VERSION}"
+    func_oss_utils_credential_init "${VAR_ALIYUN_OSS_ENDPOINT}" "${ACCESS_KEY_ID}" "${ACCESS_KEY_SECRET}"
     ;;
+repo_init)
+    # create basic repo directory structure
+    # useful when a new repo added
+    func_repo_init /tmp
+    ;;
+repo_backup)
+    func_repo_backup "${VAR_ALIYUN_OSS_BUCKET}" "centos" "${DATE_TAG}"
+    ;;
+repo_clone)
+    func_repo_clone "${VAR_ALIYUN_OSS_BUCKET}" "centos" /tmp
+    ;;
+repo_metadata_rebuild)
+    func_repo_metadata_rebuild /tmp/centos
+    ;;
+repo_upload)
+    func_repo_upload /tmp/centos "${VAR_ALIYUN_OSS_BUCKET}" "centos"
+    ;;
+repo_backup_remove)
+    func_repo_backup_remove "${VAR_ALIYUN_OSS_BUCKET}" "centos" "${DATE_TAG}"
+    ;;
+rpm_gpg_sign)
+    func_gpg_config_init
+    func_gpg_key_load /tmp/rpm-gpg-publish.private /tmp/rpm-gpg-publish.passphrase
+    rpmsign --addsign /tmp/centos/7/x86_64/Packages/${TARGET_APP}-${TAG_VERSION}-0.el7.x86_64.rpm
+    ;;
+*)
+    echo "Unknown method!"
 esac
