16a91eef40fd3cda485aa1f53f5bce8b79e809ce
soulbird <zhaothree@gmail.com>
2022-06-14 10:42:28 +0800

    fix conflict

--- build-apisix-base.sh
+++ build-apisix-base.sh
@@ -13,9 +13,9 @@ if ([ $# -gt 0 ] && [ "$1" == "latest" ]) || [ "$version" == "latest" ]; then
     debug_args="--with-debug"
     OR_PREFIX=${OR_PREFIX:="/usr/local/openresty-debug"}
 else
-    ngx_multi_upstream_module_ver="1.0.1"
+    ngx_multi_upstream_module_ver="1.1.1"
     mod_dubbo_ver="1.0.2"
-    apisix_nginx_module_ver="1.8.0"
+    apisix_nginx_module_ver="1.9.0"
     wasm_nginx_module_ver="0.6.0"
     lua_var_nginx_module_ver="v0.5.2"
     debug_args=${debug_args:-}
