6dc3ead493647a487ba8932fdf993f99c92565e0
spacewander <spacewanderlzx@gmail.com>
2022-07-27 09:43:19 +0800

    feat: prepare to release APISIX-Base 1.21.4.1.1
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- build-apisix-base.sh
+++ build-apisix-base.sh
@@ -13,11 +13,11 @@ if ([ $# -gt 0 ] && [ "$1" == "latest" ]) || [ "$version" == "latest" ]; then
     debug_args="--with-debug"
     OR_PREFIX=${OR_PREFIX:="/usr/local/openresty-debug"}
 else
-    ngx_multi_upstream_module_ver="1.1.0"
+    ngx_multi_upstream_module_ver="1.1.1"
     mod_dubbo_ver="1.0.2"
     apisix_nginx_module_ver="1.9.0"
-    wasm_nginx_module_ver="0.6.1"
-    lua_var_nginx_module_ver="v0.5.2"
+    wasm_nginx_module_ver="0.6.2"
+    lua_var_nginx_module_ver="v0.5.3"
     debug_args=${debug_args:-}
     OR_PREFIX=${OR_PREFIX:="/usr/local/openresty"}
 fi
