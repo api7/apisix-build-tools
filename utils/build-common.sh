cbaa07dca421ebef116ea6044442ea134ca8ba3a
revolyssup <ashishjaitiwari15112000@gmail.com>
2023-05-18 21:11:00 +0530

    add xz in yum install
    Signed-off-by: revolyssup <ashishjaitiwari15112000@gmail.com>

--- utils/build-common.sh
+++ utils/build-common.sh
@@ -21,7 +21,7 @@ build_apisix_base_rpm() {
     else
         dnf install -y yum-utils
         yum update --disablerepo=* --enablerepo=ubi-8-appstream-rpms --enablerepo=ubi-8-baseos-rpms -y && rm -rf /var/cache/yum
-        yum -y install --disablerepo=* --enablerepo=ubi-8-appstream-rpms --enablerepo=ubi-8-baseos-rpms gcc gcc-c++ patch wget git make sudo
+        yum -y install --disablerepo=* --enablerepo=ubi-8-appstream-rpms --enablerepo=ubi-8-baseos-rpms gcc gcc-c++ patch wget git make sudo xz
     fi
 
     command -v gcc
