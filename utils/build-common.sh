6d733e706bca31543fde51eaa84667f6b5902318
revolyssup <ashishjaitiwari15112000@gmail.com>
2023-05-18 15:45:06 +0530

    add gcc installation step on ubi apisix-base
    Signed-off-by: revolyssup <ashishjaitiwari15112000@gmail.com>

--- utils/build-common.sh
+++ utils/build-common.sh
@@ -18,6 +18,9 @@ build_apisix_base_rpm() {
         set +eu
         source /opt/rh/gcc-toolset-9/enable
         set -eu
+    else
+        yum update --disablerepo=* --enablerepo=ubi-8-appstream-rpms --enablerepo=ubi-8-baseos-rpms -y && rm -rf /var/cache/yum
+        yum -y install --disablerepo=* --enablerepo=ubi-8-appstream-rpms --enablerepo=ubi-8-baseos-rpms gcc gcc-c++ patch wget git make sudo
     fi
 
     command -v gcc
