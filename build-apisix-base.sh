576b86667e33a24ff2387486b89250bcdc8e3048
Sarasa Kisaragi <lingsamuelgrace@gmail.com>
2023-09-25 16:24:46 +0800

    change: remove amesh from apisix-base (#318)

--- build-apisix-base.sh
+++ build-apisix-base.sh
@@ -16,7 +16,6 @@ if ([ $# -gt 0 ] && [ "$1" == "latest" ]) || [ "$version" == "latest" ]; then
     wasm_nginx_module_ver="main"
     lua_var_nginx_module_ver="master"
     grpc_client_nginx_module_ver="main"
-    amesh_ver="main"
     debug_args="--with-debug"
     OR_PREFIX=${OR_PREFIX:="/usr/local/openresty-debug"}
 else
@@ -26,7 +25,6 @@ else
     wasm_nginx_module_ver="0.6.5"
     lua_var_nginx_module_ver="v0.5.3"
     grpc_client_nginx_module_ver="v0.4.3"
-    amesh_ver="main"
     debug_args=${debug_args:-}
     OR_PREFIX=${OR_PREFIX:="/usr/local/openresty"}
 fi
@@ -85,14 +83,6 @@ else
     git clone --depth=1 -b $grpc_client_nginx_module_ver \
         https://github.com/api7/grpc-client-nginx-module \
         grpc-client-nginx-module-${grpc_client_nginx_module_ver}
-fi
-
-if [ "$repo" == amesh ]; then
-    cp -r "$prev_workdir" ./amesh-${amesh_ver}
-else
-    git clone --depth=1 -b $amesh_ver \
-        https://github.com/api7/amesh \
-        amesh-${amesh_ver}
 fi
 
 cd ngx_multi_upstream_module-${ngx_multi_upstream_module_ver} || exit 1
@@ -194,10 +184,6 @@ cd ..
 
 cd grpc-client-nginx-module-${grpc_client_nginx_module_ver} || exit 1
 sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
-cd ..
-
-cd amesh-${amesh_ver} || exit 1
-sudo OPENRESTY_PREFIX="$OR_PREFIX" sh -c 'PATH="${PATH}:/usr/local/go/bin" make install'
 cd ..
 
 # package etcdctl
