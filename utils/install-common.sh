7674128081f677f60a0001a707db7b342ef359fa
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2023-10-25 15:13:44 +0530

    fix ci
    Signed-off-by: Ashish Tiwari <ashishjaitiwari15112000@gmail.com>

--- utils/install-common.sh
+++ utils/install-common.sh
@@ -25,7 +25,7 @@ install_openssl_3(){
     make -j $(nproc)
     make install
     OPENSSL_PREFIX=$(pwd)
-    export LD_LIBRARY_PATH=$OPENSSL_PREFIX/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+    export LD_LIBRARY_PATH=$OPENSSL_PREFIX${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
     cd ..
 }
 
