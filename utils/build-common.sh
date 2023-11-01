9bfb0e0c15c11667469dc5844d1e3c31df76c4d4
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2023-11-01 11:16:12 +0530

    feat: Sync openssl3 branch with master (#347)
    * feat: support new package APISIX-runtime (#342)
    
    Signed-off-by: Sn0rt <wangguohao.2009@gmail.com>
    
    * feat: upload apisix-runtime artifact (#343)
    
    Signed-off-by: Sn0rt <wangguohao.2009@gmail.com>
    
    * fix: upload apisix-runtime deb to cos (#344)
    
    Signed-off-by: Sn0rt <wangguohao.2009@gmail.com>
    
    * fix: support apisix-runtime debug (#346)
    
    Signed-off-by: Sn0rt <wangguohao.2009@gmail.com>
    
    ---------
    
    Signed-off-by: Sn0rt <wangguohao.2009@gmail.com>
    Co-authored-by: Sn0rt <wangguohao.2009@gmail.com>

--- utils/build-common.sh
+++ utils/build-common.sh
@@ -67,6 +67,68 @@ build_apisix_base_apk() {
     ${BUILD_PATH}/build-apisix-base.sh
 }
 
+build_apisix_runtime_rpm() {
+    if [[ $(rpm --eval '%{centos_ver}') == "7" ]]; then
+        yum -y install centos-release-scl
+        yum -y install devtoolset-9 patch wget git make sudo
+        set +eu
+        source scl_source enable devtoolset-9
+        set -eu
+    elif [[ $(rpm --eval '%{centos_ver}') == "8" ]]; then
+        dnf install -y gcc-toolset-9-toolchain patch wget git make sudo
+        dnf install -y yum-utils
+        set +eu
+        source /opt/rh/gcc-toolset-9/enable
+        set -eu
+    else
+        dnf install -y yum-utils
+        yum -y install --disablerepo=* --enablerepo=ubi-8-appstream-rpms --enablerepo=ubi-8-baseos-rpms gcc gcc-c++ patch wget git make sudo xz
+    fi
+
+    command -v gcc
+    gcc --version
+
+    yum-config-manager --add-repo https://openresty.org/package/centos/openresty.repo
+    yum -y install openresty-openssl111-devel openresty-pcre-devel openresty-zlib-devel
+
+    export_openresty_variables
+    ${BUILD_PATH}/build-apisix-runtime.sh
+}
+
+build_apisix_runtime_deb() {
+    arch_path=""
+    if [[ $ARCH == "arm64" ]] || [[ $ARCH == "aarch64" ]]; then
+        arch_path="arm64/"
+    fi
+    DEBIAN_FRONTEND=noninteractive apt-get update --fix-missing
+    DEBIAN_FRONTEND=noninteractive apt-get install -y sudo git libreadline-dev lsb-release libssl-dev perl build-essential
+    DEBIAN_FRONTEND=noninteractive apt-get -y install --no-install-recommends wget gnupg ca-certificates
+    wget -O - https://openresty.org/package/pubkey.gpg | apt-key add -
+
+    if [[ $IMAGE_BASE == "ubuntu" ]]; then
+        echo "deb http://openresty.org/package/${arch_path}ubuntu $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/openresty.list
+    fi
+
+    if [[ $IMAGE_BASE == "debian" ]]; then
+        echo "deb http://openresty.org/package/${arch_path}debian $(lsb_release -sc) openresty" | tee /etc/apt/sources.list.d/openresty.list
+    fi
+
+    DEBIAN_FRONTEND=noninteractive apt-get update
+    DEBIAN_FRONTEND=noninteractive apt-get install -y openresty-openssl111-dev openresty-pcre-dev openresty-zlib-dev
+
+    export_openresty_variables
+    # fix OR_PREFIX
+    if [[ $build_latest == "latest" ]]; then
+        unset OR_PREFIX
+    fi
+    ${BUILD_PATH}/build-apisix-runtime.sh ${build_latest}
+}
+
+build_apisix_runtime_apk() {
+    export_openresty_variables
+    ${BUILD_PATH}/build-apisix-runtime.sh
+}
+
 export_openresty_variables() {
     export openssl_prefix=/usr/local/openresty/openssl111
     export zlib_prefix=/usr/local/openresty/zlib
@@ -88,5 +150,14 @@ build_apisix_base_deb)
     ;;
 build_apisix_base_apk)
     build_apisix_base_apk
+    ;;
+build_apisix_runtime_rpm)
+    build_apisix_runtime_rpm
+    ;;
+build_apisix_runtime_deb)
+    build_apisix_runtime_deb
+    ;;
+build_apisix_runtime_apk)
+    build_apisix_runtime_apk
     ;;
 esac
