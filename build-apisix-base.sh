6bbc74b7461964366fda973888ed34683cde3b2a
Ling Samuel (WSL) <lingsamuelgrace@gmail.com>
2023-11-06 09:16:03 +0800

    feat: add lua-resty-events module
    Signed-off-by: Ling Samuel (WSL) <lingsamuelgrace@gmail.com>

--- build-apisix-base.sh
+++ build-apisix-base.sh
@@ -16,6 +16,7 @@ if ([ $# -gt 0 ] && [ "$1" == "latest" ]) || [ "$version" == "latest" ]; then
     wasm_nginx_module_ver="main"
     lua_var_nginx_module_ver="master"
     grpc_client_nginx_module_ver="main"
+    lua_resty_events_ver="main"
     debug_args="--with-debug"
     OR_PREFIX=${OR_PREFIX:="/usr/local/openresty-debug"}
 else
@@ -25,6 +26,7 @@ else
     wasm_nginx_module_ver="0.6.5"
     lua_var_nginx_module_ver="v0.5.3"
     grpc_client_nginx_module_ver="v0.4.4"
+    lua_resty_events_ver="0.2.0"
     debug_args=${debug_args:-}
     OR_PREFIX=${OR_PREFIX:="/usr/local/openresty"}
 fi
@@ -85,6 +87,14 @@ else
         grpc-client-nginx-module-${grpc_client_nginx_module_ver}
 fi
 
+if [ "$repo" == lua-resty-events ]; then
+    cp -r "$prev_workdir" ./lua-resty-events-${lua_resty_events_ver}
+else
+    git clone --depth=1 -b $lua_resty_events_ver \
+        https://github.com/Kong/lua-resty-events.git \
+        lua-resty-events-${lua_resty_events_ver}
+fi
+
 cd ngx_multi_upstream_module-${ngx_multi_upstream_module_ver} || exit 1
 ./patch.sh ../openresty-${OPENRESTY_VERSION}
 cd ..
@@ -140,6 +150,7 @@ fi
     --add-module=../wasm-nginx-module-${wasm_nginx_module_ver} \
     --add-module=../lua-var-nginx-module-${lua_var_nginx_module_ver} \
     --add-module=../grpc-client-nginx-module-${grpc_client_nginx_module_ver} \
+    --add-module=../lua-resty-events-${lua_resty_events_ver} \
     --with-poll_module \
     --with-pcre-jit \
     --without-http_rds_json_module \
@@ -183,6 +194,10 @@ sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
 cd ..
 
 cd grpc-client-nginx-module-${grpc_client_nginx_module_ver} || exit 1
+sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
+cd ..
+
+cd lua-resty-events-${lua_resty_events_ver} || exit 1
 sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
 cd ..
 
