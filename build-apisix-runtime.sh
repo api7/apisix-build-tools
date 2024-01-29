0a1a3637c11cede4628acf51bef7d9b2803e8758
zll600 <3400692417@qq.com>
2024-01-29 17:01:32 +0800

    fix: use zll600 repo

--- build-apisix-runtime.sh
+++ build-apisix-runtime.sh
@@ -92,7 +92,7 @@ if [ "$repo" == ngx_multi_upstream_module ]; then
     cp -r "$prev_workdir" ./ngx_multi_upstream_module-${ngx_multi_upstream_module_ver}
 else
     git clone --depth=1 -b upgrade_openresty-1.25.3 \
-        https://github.com/api7/ngx_multi_upstream_module.git \
+        https://github.com/zll600/ngx_multi_upstream_module.git \
         ngx_multi_upstream_module-${ngx_multi_upstream_module_ver}
 fi
 
