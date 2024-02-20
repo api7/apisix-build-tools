df7bebdfd0bdb3b07d3875c4101e66be5914145b
Peter Zhu <starszcan@gmail.com>
2024-02-20 15:58:29 +0800

    fix: dashboard ci (#373)

--- utils/install-common.sh
+++ utils/install-common.sh
@@ -158,6 +158,8 @@ install_dashboard() {
     go env -w GOPROXY="${goproxy}"
     cd /tmp/
     cd /apisix-dashboard
+    # FIXME: when the certificate is valid
+    yarn config set "strict-ssl" false -g
     make build
     # copy the compiled files to the specified directory for packaging
     cp -r output/* /tmp/build/output/apisix/dashboard/usr/local/apisix/dashboard
