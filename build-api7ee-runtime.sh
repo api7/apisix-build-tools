0da389bbc4720324a8c1848a89964511292b3c65
rongxin <rongxin@api7.ai>
2024-12-13 15:15:47 +0800

    remove grpc

--- build-api7ee-runtime.sh
+++ build-api7ee-runtime.sh
@@ -22,12 +22,11 @@ ld_opt=${ld_opt:-"-L$zlib_prefix/lib -L$pcre_prefix/lib -L$OPENSSL_PREFIX/lib -W
 # dependencies for building openresty
 OPENSSL_VERSION=${OPENSSL_VERSION:-"3.2.3"}
 OPENRESTY_VERSION="1.25.3.2"
-ngx_multi_upstream_module_ver="1.1.1"
+ngx_multi_upstream_module_ver="1.2.0"
 mod_dubbo_ver="1.0.2"
 apisix_nginx_module_ver="1.16.3"
 wasm_nginx_module_ver="0.7.0"
 lua_var_nginx_module_ver="v0.5.3"
-grpc_client_nginx_module_ver="v0.4.4"
 lua_resty_events_ver="0.2.0"
 
 
@@ -130,13 +129,6 @@ else
         lua-var-nginx-module-${lua_var_nginx_module_ver}
 fi
 
-if [ "$repo" == grpc-client-nginx-module ]; then
-    cp -r "$prev_workdir" ./grpc-client-nginx-module-${grpc_client_nginx_module_ver}
-else
-    git clone --depth=1 -b $grpc_client_nginx_module_ver \
-        https://github.com/api7/grpc-client-nginx-module \
-        grpc-client-nginx-module-${grpc_client_nginx_module_ver}
-fi
 
 cd ngx_multi_upstream_module-${ngx_multi_upstream_module_ver} || exit 1
 ./patch.sh ../openresty-${OPENRESTY_VERSION}
@@ -152,9 +144,6 @@ cd ..
 
 luajit_xcflags=${luajit_xcflags:="-DLUAJIT_NUMMODE=2 -DLUAJIT_ENABLE_LUA52COMPAT"}
 no_pool_patch=${no_pool_patch:-}
-# TODO: remove old NGX_HTTP_GRPC_CLI_ENGINE_PATH once we have released a new
-# version of grpc-client-nginx-module
-grpc_engine_path="-DNGX_GRPC_CLI_ENGINE_PATH=$OR_PREFIX/libgrpc_engine.so -DNGX_HTTP_GRPC_CLI_ENGINE_PATH=$OR_PREFIX/libgrpc_engine.so"
 
 cd openresty-${OPENRESTY_VERSION} || exit 1
 
@@ -172,7 +161,7 @@ fi
 
 
 ./configure --prefix="$OR_PREFIX" \
-    --with-cc-opt="-DAPI7EE_RUNTIME_VER=$runtime_version $grpc_engine_path $cc_opt" \
+    --with-cc-opt="-DAPI7EE_RUNTIME_VER=$runtime_version $cc_opt" \
     --with-ld-opt="-Wl,-rpath,$OR_PREFIX/wasmtime-c-api/lib $ld_opt" \
     $debug_args \
     --add-module=../mod_dubbo-${mod_dubbo_ver} \
@@ -182,7 +171,6 @@ fi
     --add-module=../apisix-nginx-module-${apisix_nginx_module_ver}/src/meta \
     --add-module=../wasm-nginx-module-${wasm_nginx_module_ver} \
     --add-module=../lua-var-nginx-module-${lua_var_nginx_module_ver} \
-    --add-module=../grpc-client-nginx-module-${grpc_client_nginx_module_ver} \
     --add-module=../lua-resty-events-${lua_resty_events_ver} \
     --with-poll_module \
     --with-pcre-jit \
@@ -231,10 +219,6 @@ sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
 cd ..
 
 cd wasm-nginx-module-${wasm_nginx_module_ver} || exit 1
-sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
-cd ..
-
-cd grpc-client-nginx-module-${grpc_client_nginx_module_ver} || exit 1
 sudo OPENRESTY_PREFIX="$OR_PREFIX" make install
 cd ..
 
