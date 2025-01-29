c595b7a158f6107a1802b4f405a2aaa88432eee4
Abhishek Choudhary <shreemaan.abhishek@gmail.com>
2025-01-29 18:09:24 +0545

    modify apisix-base script

--- build-apisix-base.sh
+++ build-apisix-base.sh
@@ -4,9 +4,9 @@ set -x
 
 version=${version:-0.0.0}
 
-OPENRESTY_VERSION=${OPENRESTY_VERSION:-1.25.3.2}
+OPENRESTY_VERSION=${OPENRESTY_VERSION:-1.27.1.1}
 if [ "$OPENRESTY_VERSION" == "source" ] || [ "$OPENRESTY_VERSION" == "default" ]; then
-    OPENRESTY_VERSION="1.25.3.2"
+    OPENRESTY_VERSION="1.27.1.1"
 fi
 
 if ([ $# -gt 0 ] && [ "$1" == "latest" ]) || [ "$version" == "latest" ]; then
@@ -19,9 +19,9 @@ if ([ $# -gt 0 ] && [ "$1" == "latest" ]) || [ "$version" == "latest" ]; then
     debug_args="--with-debug"
     OR_PREFIX=${OR_PREFIX:="/usr/local/openresty-debug"}
 else
-    ngx_multi_upstream_module_ver="1.2.0"
+    ngx_multi_upstream_module_ver="12711"
     mod_dubbo_ver="1.0.2"
-    apisix_nginx_module_ver="1.16.2"
+    apisix_nginx_module_ver="12711"
     wasm_nginx_module_ver="0.7.0"
     lua_var_nginx_module_ver="v0.5.3"
     lua_resty_events_ver="0.2.0"
